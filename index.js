dc708aee5fbc662e2a73fe0a141ccd0b2faceb92
Hyung Kim <hjkmines@gmail.com>
2020-08-13 23:13:44 -0600

    adds function of condition 1

--- index.js
+++ index.js
@@ -4,24 +4,19 @@ let items = [["sdfoiajsd", 4, 3], ["test", 1, 2], ["sam", 3, 6], ["samsdfdytjs",
 function fetchItemsToDisplay(items, sortParameter, sortOrder, itemsPerPage, pageNumber) {
     let sortedItems = items 
 
-    function CompareAscByRel(a, b) {
-        if (a[1] < b[1]) return -1;
-        if (a[1] > b[1]) return 1;
-        return 0;
-    }
-
+    
     function CompareDscByRel(a, b) {
         if (a[1] < b[1]) return 1;
         if (a[1] > b[1]) return -1;
         return 0;
     }
-
+    
     function CompareAscByPrice(a, b) {
         if (a[2] < b[2]) return -1;
         if (a[2] > b[2]) return 1;
         return 0;
     }
-
+    
     function CompareDscByPrice(a, b) {
         if (a[2] < b[2]) return 1;
         if (a[2] > b[2]) return -1;
@@ -37,18 +32,23 @@ function fetchItemsToDisplay(items, sortParameter, sortOrder, itemsPerPage, page
     } else if (sortParameter === 2 && sortOrder === 1) {
         sortedItems = sortedItems.sort(CompareDscByPrice)
     }
-
+    
     const collectedItems = sortedItems.slice((pageNumber - 1) * itemsPerPage, pageNumber * itemsPerPage)
-
+    
     const itemNames = []
-
+    
     for (let i = 0; i < collectedItems.length; i++) {
         itemNames.push(collectedItems[i][0])
     }
-
+    
     return itemNames; 
-
+    
 }
 
+function CompareAscByRel(a, b) {
+    if (a[1] < b[1]) return -1;
+    if (a[1] > b[1]) return 1;
+    return 0;
+}
 
 fetchItemsToDisplay(items, 1, 0, 2, 2)
